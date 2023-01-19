88fb71ff5badf82d924550399c9ef465d68e55b8
theaarc <ambassiraambassira@gmail.com>
2023-01-19 13:22:28 +0100

    third push
    some more changes from type_doyen

--- route/type_doyen_route.js
+++ route/type_doyen_route.js
@@ -4,7 +4,7 @@ const type_doyen_controller =require('../controller/type_doyen_controller');
 const authenticate =require('../MiddleWares/Authenticate')
 
 router.get('/',type_doyen_controller.index);
-router.post('/register',type_doyen_controller.store);
+router.post('/store',type_doyen_controller.store);
 router.get('/show',type_doyen_controller.show);
 router.post('/update',type_doyen_controller.update);
 router.post('/destroy',type_doyen_controller.destroy);
